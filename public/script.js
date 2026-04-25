4f2bcff7f41d04964170aebd6f7f6f2592dc5fc8
H4elme <atmrosh@gmail.com>
2026-04-25 21:00:21 +0200

    Implement adding flights

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-async function refreshFlights() {
+async function refresh_flights() {
     try {
         const response = await fetch('/api/flights');
         const flights = await response.json();
@@ -17,16 +17,16 @@ async function refreshFlights() {
             <td>${new Date(flight.arrival_time).toLocaleString()}</td>
             <td>${flight.plane}</td>
             <td>${flight.crew_member}</td>
-            <td><button onclick = "deleteFlight(${flight.id})">Delete</button></td>
+            <td><button onclick = 'delete_flight(${flight.id})'>Delete</button></td>
             `;
             tableBody.appendChild(row);
         });
     } catch (err) {
-        console.error("Frontend error: ", err);
+        console.error('Frontend error: ', err);
     }
 }
 
-async function deleteFlight(id) {
+async function delete_flight(id) {
     if (!confirm(`Are you sure you want to delete flight #${id}?`)) {
         return;
     }
@@ -36,16 +36,80 @@ async function deleteFlight(id) {
         });
 
         if (response.ok) {
-            alert("Flight deleted!");
-            refreshFlights();
+            alert('Flight deleted!');
+            refresh_flights();
         } else {
             const errorData = await response.json();
-            alert(`Error ${errorData.message || "Could not delete"}`);
+            alert(`Error ${errorData.message || 'Could not delete'}`);
         }
     } catch (err) {
         await pool.query('ROLLBACK');
-        console.error("Frontend error: ", err);
+        console.error('Frontend error: ', err);
     }
 }
 
-refreshFlights();
+document.getElementById('add_flight_btn').addEventListener('click', function() {
+    document.getElementById('add_form').style.display = 'block';
+});
+
+document.getElementById('add_flight_btn').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = 'block';
+});
+
+document.getElementById('close_btn').addEventListener('click', function() {
+    close_form();
+});
+document.getElementById('close_btn').addEventListener('click', function() {
+    close_form();
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    close_form();
+});
+
+document.getElementById('add_btn').addEventListener('click', function() {
+    add_flight();
+});
+
+async function close_form() {
+    document.getElementById('add_form').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+async function add_flight() {
+    try {
+        const depart_from_input = document.getElementById('depart_from_input').value;
+        const destination_input = document.getElementById('destination_input').value;
+        const departure_time_input = document.getElementById('departure_time_input').value;
+        const arrival_time_input = document.getElementById('arrival_time_input').value;
+        const plane_id_input = document.getElementById('plane_id_input').value;
+        const crew_id_input = document.getElementById('crew_id_input').value;
+        console.log("AAAA", depart_from_input, crew_id_input);
+        const response = await fetch('/api/flights', {
+            method: 'POST',
+            headers: new Headers({'Content-Type':'application/json'}),
+            body: JSON.stringify({
+                'depart_from': depart_from_input,
+                'destination': destination_input,
+                'departure_time': departure_time_input,
+                'arrival_time': arrival_time_input,
+                'plane_id': plane_id_input,
+                'crew_id': crew_id_input
+            })
+        });
+        if (response.ok) {
+            alert("Flight added.");
+            refresh_flights();
+            close_form();
+        }
+            else {
+                console.error("Frontend error.");
+            }
+    } catch (err) {
+        console.log("Error: ", err);
+    }
+}
+
+refresh_flights();
+
+// TODO: change delete_btn to eventListener, let POST take multiple crew members, add error messages to add_flight
